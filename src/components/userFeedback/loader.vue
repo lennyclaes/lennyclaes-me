b34bf33d64122224d64fa13a5a4491887dfe4393
Lenny Claes <lenny.claes@gmail.com>
2021-11-18 18:25:57 +0100

    Initial commit

---
+++ src/components/userFeedback/loader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="loader"></div>
+</template>
+
+<script>
+export default {
+  name: "loader",
+};
+</script>
+
+<style lang="scss" scoped>
+.loader {
+  width: 15px;
+  height: 15px;
+  background: black;
+  border-radius: 90px;
+  animation: loading 1s linear infinite alternate;
+}
+
+@keyframes loading {
+  0% {
+    transform: translateX(-40px);
+    width: 15px;
+  }
+  50% {
+    width: 80px;
+  }
+  100% {
+    transform: translateX(40px);
+    width: 15px;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home" :class="'home-'+background">
+    <h1 class="heading">Working on the future</h1>
+    <h2 class="tagline">Hold on</h2>
+    <loader />
   </div>
 </template>
 
 <script>
+import loader from '../components/userFeedback/loader.vue';
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    loader
+  },
+  data: () => {
+    return {
+      background: Math.floor(Math.random() * 4) + 1
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+
+  &-1 {
+    background: #c44a4a;
+  }
+  &-2 {
+    background: #71c44a;
+  }
+  &-3 {
+    background: #4a8dc4;
+  }
+  &-4 {
+    background: #c44aaa;
   }
 }
-</script>
+
+.heading {
+  font-size: 72px;
+  font-family: "Catamaran", Arial, Helvetica, sans-serif;
+  text-align: center;
+}
+
+.tagline {
+  font-size: 32px;
+  font-family: "Catamaran", Arial, Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
